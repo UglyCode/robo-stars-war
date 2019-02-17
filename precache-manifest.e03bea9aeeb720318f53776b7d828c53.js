22416e391929b5deaf2ee35ae7fe0c84043dd27f
uglyCode <appostnikov@gmail.com>
2019-02-17 14:15:32 +0300

    Updates

---
+++ precache-manifest.e03bea9aeeb720318f53776b7d828c53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6f9adb851fe1ef6672749e8dc6257c",
+    "url": "/robo-stars-war/static/media/star-background-star-wars-7.6f6f9adb.jpg"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/robo-stars-war/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-stars-war/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6b625a58a103d8242c0e",
+    "url": "/robo-stars-war/static/js/runtime~main.6b625a58.js"
+  },
+  {
+    "revision": "e961c350b28692dbd68e",
+    "url": "/robo-stars-war/static/js/main.e961c350.chunk.js"
+  },
+  {
+    "revision": "d1402e48347a4072ed72",
+    "url": "/robo-stars-war/static/js/2.d1402e48.chunk.js"
+  },
+  {
+    "revision": "e961c350b28692dbd68e",
+    "url": "/robo-stars-war/static/css/main.4a193a7c.chunk.css"
+  },
+  {
+    "revision": "d1402e48347a4072ed72",
+    "url": "/robo-stars-war/static/css/2.5478547b.chunk.css"
+  },
+  {
+    "revision": "767bb0c9c3063cc1787098f429cb40c1",
+    "url": "/robo-stars-war/index.html"
+  }
+];
